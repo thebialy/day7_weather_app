ee4e1ddd8c3fb42211e30a6c78c9cefa690423dc
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-12 09:36:58 -0600

    created react app for weather

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const api = {
+  key: "f01500f5e1b6f459334d86739fccf2ae",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+
+function App() {
+  return (
+    <div className="app">
+      Hello
+    </div>
+  );
+}
+
+export default App;
