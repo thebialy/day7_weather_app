adc9a0c16f49293e14bfc9bdd2d082ac790fe77b
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-14 15:11:49 -0600

    made a standard background before the city and temp are known

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
   }
 
   return (
-    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app cold') : 'app'}>
       <main>
         <div className="title"> Whats the weather in your city?</div>
         <div className="search-box">
